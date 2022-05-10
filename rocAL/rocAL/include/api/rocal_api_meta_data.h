2080182e44ac2c9e915419daafaa3ca48c591d70
SundarRajan28 <sundarrajan@multicorewareinc.com>
2022-05-10 12:32:39 -0400

    Adding header definitions for mask functions

--- rocAL/rocAL/include/api/rocal_api_meta_data.h
+++ rocAL/rocAL/include/api/rocal_api_meta_data.h
@@ -141,6 +141,13 @@ extern "C" void ROCAL_API_CALL rocalGetImageSizes(RocalContext rocal_context, in
 
 ///
 /// \param rocal_context
+/// \param image_idx the imageIdx in the output batch
+/// \return The size of the buffer needs to be provided by user to get mask box info associated with image_idx in the output batch.
+extern "C" unsigned ROCAL_API_CALL rocalGetMaskCount(RocalContext rocal_context, int* buf );
+extern "C" void ROCAL_API_CALL rocalGetMaskCoordinates(RocalContext rocal_context, int* bufcount, float* buf);
+
+///
+/// \param rocal_context
 /// \param source_path path to the file that contains the metadata file
 /// \param filename_prefix: look only files with prefix ( needed for cifar10)
 /// \return RocalMetaData object, can be used to inquire about the rocal's output (processed) tensors
