0e80cf0cae99f983550deda468b7bbda7f43d66f
SundarRajan28 <sundarrajan@multicorewareinc.com>
2023-05-25 10:02:10 +0000

    Adding wait timers to rocAL

--- rocAL/rocAL/source/api/rocal_api_info.cpp
+++ rocAL/rocAL/source/api/rocal_api_info.cpp
@@ -122,9 +122,9 @@ TimingInfo
     auto info = context->timing();
     // INFO("bbencode time "+ TOSTR(info.bb_process_time)); //to display time taken for bbox encoder
     if (context->master_graph->is_video_loader())
-        return {info.video_read_time, info.video_decode_time, info.video_process_time, info.copy_to_output};
+        return {info.video_read_time, info.video_decode_time, info.video_process_time, info.copy_to_output, info.wait_if_empty_time, info.wait_if_full_time, info.circular_buffer_wait_if_empty_time, info.circular_buffer_wait_if_full_time};
     else
-        return {info.image_read_time, info.image_decode_time, info.image_process_time, info.copy_to_output};
+        return {info.image_read_time, info.image_decode_time, info.image_process_time, info.copy_to_output, info.wait_if_empty_time, info.wait_if_full_time, info.circular_buffer_wait_if_empty_time, info.circular_buffer_wait_if_full_time};
 }
 
 RocalMetaData
